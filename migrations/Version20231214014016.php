bed7d51b7af1159cd9bcb50b5c7cd682e6620704
TonyHuynh16031999 <138096146+TonyHuynh16031999@users.noreply.github.com>
2023-12-18 07:56:01 +0700

    This is MySemiProject

---
+++ migrations/Version20231214014016.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20231214014016 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE product ADD product_name VARCHAR(255) DEFAULT NULL, ADD price INT DEFAULT NULL, ADD description VARCHAR(1000) DEFAULT NULL, ADD image VARCHAR(255) DEFAULT NULL, ADD create_date DATETIME DEFAULT NULL, ADD notes VARCHAR(255) DEFAULT NULL');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE product DROP product_name, DROP price, DROP description, DROP image, DROP create_date, DROP notes');
+    }
+}
